f1b62b19a9b66818a0034713c24b7440566ec42b
Nien <wolfzxcv@gmail.com>
2020-02-19 17:49:28 +0100

    dash products basic layout

--- src/components/Dashboard/DashNav.vue
+++ src/components/Dashboard/DashNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Pokemon shop</a>
       <input
         class="form-control form-control-dark w-100"
         type="text"
--- src/components/Dashboard/DashProducts.vue
+++ src/components/Dashboard/DashProducts.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>product</div>
+  <div>
+    <div class="text-right mt-4">
+      <button class="btn btn-primary">New Product</button>
+    </div>
+
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th width="150">Category</th>
+          <th>Name</th>
+          <th width="100">Was</th>
+          <th width="100">Price</th>
+          <th width="100">Enabled</th>
+          <th width="100">Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="x in products" :key="x.id">
+          <td>{{ x.category }}</td>
+          <td>{{ x.title }}</td>
+          <td class="text-right">{{ x.origin_price }}</td>
+          <td class="text-right">{{ x.price }}</td>
+          <td>
+            <span v-if="x.is_enabled" class="text-success">enabled</span
+            ><span v-else>disabled</span>
+          </td>
+          <td><button class="btn btn-outline-primary btn-sm">Edit</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
