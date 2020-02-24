a271c21a0502d4913fca4300df0b53870737ec30
Nien <wolfzxcv@gmail.com>
2020-02-24 15:34:18 +0800

    fix path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wolfzxcv/Pokemon-shop.git master:gh-pages
+git push -f https://github.com/wolfzxcv/Pokemon-shop.git master:gh-pages
 
 cd -
