e228aaf97c1496156b0a5fe1d2e58aaa9aa253dc
Nien <wolfzxcv@gmail.com>
2020-02-24 15:12:04 +0800

    try deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
+
+# abort on errors
 set -e
-# 打包
+
+# build
 npm run build
-# 移動至到打包後的dist目錄 
+
+# navigate into the build output directory
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://github.com/wolfzxcv/Pokemon-shop.git 分支為 gh-pages
-git push -f https://github.com/wolfzxcv/Pokemon-shop.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:wolfzxcv/Pokemon-shop.git master:gh-pages
+
 cd -
